b7a845c7789665eb68f8d1c8ba633a1483647501
Antonio <a.gomezgimenez@hotmail.com>
2024-10-11 18:01:16 +0200

    modificaciones a las variables de entorno, readme y cambios menores

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
     production: true,
-    api_url: 'http://localhost:3000/'  // Url backend
+  //api_url: 'http://applabelinguco-v1-api.com/'
+  //api_url: 'http://150.214.112.231:3001/'
+    api_url: 'http://localhost:3001/'
   };
-  
+
+
